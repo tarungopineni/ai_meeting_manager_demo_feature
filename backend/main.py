bb3acf71940c4bc3183d7f591ca3b79dcf38e3a2
Tarun <tarungopineni@gmail.com>
2026-08-28 18:07:47 +0530

    fix

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,25 @@
 from fastapi import FastAPI
-from .routers import auth,users,tasks,meetings
-from .database import Base,engine
+from fastapi.middleware.cors import CORSMiddleware
+
+from .routers import auth, users, tasks, meetings
+from .database import Base, engine
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://ai-meeting-manager-f14c.vercel.app",
+        "http://localhost:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router)
 app.include_router(users.router)
 app.include_router(tasks.router)
 app.include_router(meetings.router)
 
-Base.metadata.create_all(bind = engine)
+Base.metadata.create_all(bind=engine)
